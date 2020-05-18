9cfbbebb5b012b185b41faab864dde5de23d9c0d
Lilia Anisimova <lilia.anisimova@gmail.com>
2020-05-18 19:30:33 +0800

    fix

--- client/components/header.js
+++ client/components/header.js
@@ -26,11 +26,11 @@ const Header = () => {
       </div>
       {toggled && (
         <div id="menu" className="block">
-          <a href = "https://www.google.com/">
-          <button
-            type="button"
-            className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
-          >
+          <a href="https://www.google.com/">
+            <button
+             type="button"
+              className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+            >
             <svg
               className="fill-current h-3 w-3"
               viewBox="0 0 20 20"
@@ -39,7 +39,7 @@ const Header = () => {
               <title>Menu</title>
               <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
             </svg>
-          </button>
+             </button>
           </a>
         </div>
       )}
