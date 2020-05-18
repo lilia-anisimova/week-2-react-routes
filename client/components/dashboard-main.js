a34fbf332391a2d98371257ed4046015f69c5b80
Lilia Anisimova <lilia.anisimova@gmail.com>
2020-05-18 20:54:11 +0800

    url fix

--- client/components/dashboard-main.js
+++ client/components/dashboard-main.js
@@ -6,7 +6,7 @@ const DashboardMain = () => {
   return (
     <div>
       <div id="title"> Main </div> 
-      <Link to="/dashboard/dashboard/profile/9465eaa4-071c-4ea3-a592-b54c6deb3f7f"> Go To Profile </Link>
+      <Link to="/dashboard/profile/9465eaa4-071c-4ea3-a592-b54c6deb3f7f"> Go To Profile </Link>
       <Link to="/dashboard"> Go To Root </Link>
     </div>
   )
