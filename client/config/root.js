d827a7579d7ac9d2532a226f7168a60a6f016fbb
Lilia Anisimova <lilia.anisimova@gmail.com>
2020-05-18 20:36:32 +0800

    fix

--- client/config/root.js
+++ client/config/root.js
@@ -85,7 +85,7 @@ export default (props) => {
             <Route exact path="/" component={() => <DummyView />} />
             <Route exact path="/dashboard" component={() => <Home />} />
             <Route exact path="/dashboard/profile/:user" component={() => <Home />} />
-            <Route exact path="//dashboard/main" component={() => <Home />} />
+            <Route exact path="/dashboard/main" component={() => <Home />} />
             <PrivateRouteConnected exact path="/hidden-route" component={() => <DummyView />} />
             <Route component={() => <NotFound />} />
           </Switch>
